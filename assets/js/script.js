65fcb9e5b7dd04d8780571b4147ea11e25d19eaf
OmidDeiminiyat <omid.deiminiyat@yahoo.com>
2024-06-24 09:28:15 +0200

    Done

--- assets/js/script.js
+++ assets/js/script.js
@@ -115,4 +115,31 @@ async function fetchIPAddress(domain) {
   }
   
 
+
+  // script.js
+// Get the button
+let mybutton = document.getElementById("backToTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+mybutton.onclick = function() {
+    scrollToTop();
+};
+
+function scrollToTop() {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+}
+
   
